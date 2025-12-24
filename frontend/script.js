a238cad606d187d7b64d6eb7e2c43d8ad3b158a8
VenkataNarayana Mogili <venkatanarayanamogili412@gmail.com>
2025-12-24 10:38:21 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -40,7 +40,7 @@
       try {
         setOutput('Generating summary…');
 
-        const response = await fetch('http://127.0.0.1:5000/summarize', {
+        const response = await fetch('https://summarizer-cqm5.onrender.com/summarize', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
